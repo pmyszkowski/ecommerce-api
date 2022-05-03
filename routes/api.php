3b9b2c06208a2147b52afffea13793a4a2e44530
Paweł Myszkowski <pawel.myszkowsky@gmail.com>
2022-05-03 13:10:03 +0200

    Set routes, change endpoint url

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::prefix('admin')->group(function () {
+    Route::apiResource('products', ProductController::class)->except('destroy');
+    Route::apiResource('users', UserController::class)->except('destroy');
+    Route::apiResource('orders', OrderController::class)->except('destroy');
+});
+
+Route::prefix('public')->group(function () {
+    Route::post( 'orders', [OrderController::class, 'store'] );
+    Route::get('products', [ProductController::class, 'index'] );
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
